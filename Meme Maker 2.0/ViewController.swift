6ce245492435b9540d7aa07eaaf390f678a65518
RareTimes <w.dickson@live.com>
2020-07-29 05:51:17 -0700

    Initial Commit

---
+++ Meme Maker 2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Meme Maker 2.0
+//
+//  Created by Asante on 7/29/20.
+//  Copyright © 2020 Walter Dickson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
